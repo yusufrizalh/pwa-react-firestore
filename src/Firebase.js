eec21018e2ed3407b733c1af4a5032db4a4584cc
yusufrizalh <yusufrizalh@gmail.com>
2020-03-17 16:57:26 +0700

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -4,13 +4,13 @@ import firestore from 'firebase/firestore'
 const settings = {timestampsInSnapshots: true};
 
 const config = {
-    apiKey: "AIzaSyClAyt1WLpJZsdgzZA_1x_nCBQoLhnne3Q",
-    authDomain: "react-pwa-17032020.firebaseapp.com",
-    databaseURL: "https://react-pwa-17032020.firebaseio.com",
-    projectId: "react-pwa-17032020",
-    storageBucket: "react-pwa-17032020.appspot.com",
-    messagingSenderId: "105516755048",
-    appId: "1:105516755048:web:64a1623a15a3876e6ab3dd"
+    apiKey: "**********",
+    authDomain: "**********",
+    databaseURL: "**********",
+    projectId: "**********",
+    storageBucket: "**********",
+    messagingSenderId: "**********",
+    appId: "**********"
 };
 firebase.initializeApp(config);
 
